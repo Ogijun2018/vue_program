e808fb7b53a265db834351b13b003fb3004ec887
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2020-11-30 22:52:31 +0900

    タスク管理アプリ途中(なぜかエラー)

---
+++ task_management_app/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: { // 値が変更された時とかに呼び出される
+    tasks() {
+      return this.$store.state.tasks
+    },
+  }
+}
+</script>
