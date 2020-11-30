011bf71b51974bd43b5a3132a5ec5947ec640dc1
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2020-11-30 22:23:47 +0900

    スコープ付きCSSのメリット

---
+++ scoped_style/root.vue
@@ -0,0 +1,21 @@
+<!-- スコープ付きCSSのメリット 
+  複数の単一ファイルコンポーネントを組み合わせた例 -->
+<template>
+  <div id="root">
+    <h1 class="header">Rootコンポーネント</h1>
+    <p>これはRootコンポーネントです。</p>
+    <foo/><bar/>
+  </div>
+</template>
+<script>
+import Foo from './foo'
+import Bar from './bar'
+export default {
+  components: { Foo, Bar }
+}
+</script>
+<style scoped>
+#root { border: solid 1px blue; margin: 4px; padding: 4px;}
+.header { font-size: 200%; }
+p { text-decoration: underline; }
+</style>
